a6ffde40e95e6b738b2692669595bddf9d64ca02
Franco Eusébio Garcia <francoegarcia@outlook.com>
2017-08-20 12:22:24 -0300

    Moved the files from the major-mode repository.

---
+++ snippets/godot-gdscript-mode/.yas-setup.el
@@ -0,0 +1,6 @@
+;; (add-hook godot-gdscript-mode-hook
+;;           '(lambda ()
+;;              ;;(set (make-local-variable 'yas-indent-line) 'fixed)
+;;              (set (make-local-variable
+;;                    'buffer-local-condition)
+;;                   ((not (godot-gdscript-syntax-comment-or-string-p))))))
